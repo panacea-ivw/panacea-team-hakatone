c02ee643966293cf9704fe281b6d898bfc2963e2
ivwdie <bamqufn@gmail.com>
2025-01-27 09:46:25 +0300

    kekw

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -15,13 +15,13 @@ function Navigation(){
                     <Link to="/map"> 
                         Карта загруженности 
                     </Link>
-                    <Link to="/about"> 
+                    <a href="https://www.ufanet.ru/pages/o-nas/?city_code=(800)&phone=600-97-01&utm_source=yandex&utm_medium=cpc&utm_campaign=71427021&utm_content=11749243608&utm_term=%D1%83%D1%84%D0%B0%D0%BD%D0%B5%D1%82"> 
                         О нас 
-                    </Link>
+                    </a>
                 </div>
                 <div className="tel">
                     <Link to ='/registration'>
-                        Рега
+                        Профиль
                     </Link>
                 </div>
             </div>
